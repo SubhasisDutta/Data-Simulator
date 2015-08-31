3482bb688e7aa20711adcbcfae823659bd74da9b
subhasisDutta <subhasisdutta300887@gmail.com>
2015-08-31 01:52:34 -0700

    timestamping of data

--- src/com/dgstool/common/DataGenerator.py
+++ src/com/dgstool/common/DataGenerator.py
@@ -5,32 +5,67 @@ Created on Aug 30, 2015
 '''
 
 import xml.etree.ElementTree as ET
+import csv
+import datetime
 
 class DataGenerator(object):
     '''
     This does all the processing to take an XML input and create an output that is persisted.
     '''
     
-    def __init__(self,fileName):
+    def __init__(self,inputConfigFile,resultFile):
         '''
         Constructor
         '''
-        self.inputConfigFile=fileName
+        self.inputConfigFile=inputConfigFile
+        self.resultFile=resultFile
         
     def processXMLConfig(self):
         '''
         This method parses a XML file and returns an Object that acts as the input for generating data.
         '''        
         tree = ET.parse(self.inputConfigFile)
-        root = tree.getroot()
-        print root        
+        root = tree.getroot()              
         return root
     
-    def generateData(self):  
+    def generateData(self,confObj):  
         pass 
-     
-    def process(self):
-        root=self.processXMLConfig()
+    
+    def insertToCSVFile(self,dataList,writeType):
+        with open(self.resultFile, writeType) as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(dataList)
+        return True
+   
+    def getHeading(self):
+        heading=[]
+        heading.append(self.root.find('startindex').get('name'))
+        for column in self.root.findall('column'):
+            heading.append(column.get('name'))
+        return heading
         
+    def process(self):
+        '''
+        Parcs the Config and generates the data
+        '''
+        self.root=self.processXMLConfig()
+        self.insertToCSVFile(self.getHeading(),'wb')
+        
+        print self.root.find('startindex').text
+        
+        startTime=datetime.datetime.strptime(self.root.find('startindex').text, "%Y-%m-%dT%H:%M:%S")
+        endTime=datetime.datetime.strptime(self.root.find('endindex').text, "%Y-%m-%dT%H:%M:%S")
+        timeIterator=startTime
+        count=1
+        while timeIterator < endTime:
+            record=[]
+            record.append(timeIterator)
+            #To do for other records
+            record.append('A random'+str(count))
+            record.append('B random'+str(count))
+            record.append('C random'+str(count))
+            count+=1
+            timeIterator += datetime.timedelta(milliseconds=int(self.root.find('step').text))            
+            self.insertToCSVFile(record,'ab')        
         return True
 
